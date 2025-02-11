3883cf9f5508666b878979ccf3f659bfc0f562c3
MichaellIbarra <michaell.ibarra@vallegrande.edu.pe>
2025-02-11 00:49:02 -0500

    add: msvc gateway

---
+++ msvc-gateway/src/test/java/dev/matichelo/msvc/gateway/MsvcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matichelo.msvc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
